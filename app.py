2626e13706abba77c86530896ffea283e6b85151
Stani <stanimir.dobrev1994@gmail.com>
2023-11-19 17:16:11 +0100

    refactor

--- app.py
+++ app.py
@@ -3,16 +3,23 @@ import persistencia
 
 app = Flask(__name__)
 @app.route("/pizza", methods=['POST'])
-def helloPOST():
+def pizza():
     nombre = request.form.get("nombre")
     apellidos = request.form.get("apellidos")
+    printInConsole(nombre, apellidos)
+    saveInFile(nombre, apellidos)
+   
+    return redirect("http://localhost/PizzaHouse/solicita_pedido.html", code=302)
+
+def printInConsole(nombre, apellidos):
     print(nombre)
     print(apellidos)
 
+def saveInFile(nombre, apellidos):
+    cleanFile()
+    persistencia.guardar_pedido(nombre, apellidos)
+
+def cleanFile():
     file = open("pedidos.txt", "w", encoding="utf-8")
     file.write("")
     file.close()
-
-    persistencia.guardar_pedido(nombre, apellidos)
-
-    return redirect("http://localhost/PizzaHouse/solicita_pedido.html", code=302)
