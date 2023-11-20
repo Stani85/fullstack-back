5ea81dcb927500b4587dc82692bbb6965b13e703
Stani <stanimir.dobrev1994@gmail.com>
2023-11-20 18:17:22 +0100

    poner guion delante del nombre

--- test_persistencia.py
+++ test_persistencia.py
@@ -5,10 +5,10 @@ def test_guardar_pedido():
     """ Prueba general """
     utils.clean_file()
     with open("pedidos.txt", "w+", encoding="utf-8") as file:
-        persistencia.guardar_pedido("Pedro", "Gil de Diego")
-        persistencia.guardar_pedido("Michael", "Jordan")
+        persistencia.guardar_pedido("-Pedro", "Gil de Diego")
+        persistencia.guardar_pedido("-Michael", "Jordan")
         firstline = file.readline()
         secondline = file.readline()
         file.close()
-    assert firstline == "Pedro Gil de Diego\n"
-    assert secondline == "Michael Jordan\n"
+    assert firstline == "-Pedro Gil de Diego\n"
+    assert secondline == "-Michael Jordan\n"
